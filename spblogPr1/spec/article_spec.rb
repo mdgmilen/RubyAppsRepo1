82cbd87b62a7ee1db4f814c3062121c8861d50b9
Milen Georgiev im <milen.georgiev@improve1.com>
2016-02-17 18:54:01 +0200

    two errors fixed, in models/article.rb and spec/article_spec.rb

--- spblogPr1/spec/article_spec.rb
+++ spblogPr1/spec/article_spec.rb
@@ -7,6 +7,6 @@ require 'rails_helper'
 
 describe Article do  #   ||
    it "the truth" do  # "test" keyword is used in projectRoot/test folder. Here in rspec, it should be "it"
-     assert fae 
+     assert false 
    end
 end
